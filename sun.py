04c96b8b1b49032dcfcca32a1c1d697784176262
bhanu1357 <115222249+bhanu1357@users.noreply.github.com>
2025-06-12 14:29:07 +0530

    Add files via upload

---
+++ sun.py
@@ -0,0 +1,6 @@
+sample_1= int(input("first:"))
+sample_2=int(input("second:"))
+if (sample_1 and sample_2) is range(0,100):
+    print(sample_1+sample_2)
+else:
+    print("enter the number b/w 0 to 100")
